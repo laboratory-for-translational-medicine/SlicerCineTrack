55c6b4dc347d5f97b6cc58237875d9e86fefd295
HaPhan Tran <haphantran@gmail.com>
2024-01-16 21:46:18 -0500

    add license 4 clause BSD style

--- Track/Track.py
+++ Track/Track.py
@@ -1,3 +1,23 @@
+"""
+==============================================================================
+
+  Copyright (c) 2024, laboratory-for-translational-medicine
+  Toronto Metropolitan University, Toronto, ON, Canada. All Rights Reserved.
+
+  See LISENSE.txt
+  or http://www.slicer.org/copyright/copyright.txt for details.
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+
+
+==============================================================================
+"""
+
+
 import os
 import csv
 import re
