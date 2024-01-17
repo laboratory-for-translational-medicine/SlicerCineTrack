b4cac22ac91ddc615aa2cfe833491f880d9c39f6
HaPhan Tran <haphantran@gmail.com>
2024-01-16 21:26:17 -0500

    small change in test
    Former-commit-id: 1ac1c710b95633670ef9260ffc67c466f0582ff3

--- Track/Track.py
+++ Track/Track.py
@@ -1200,7 +1200,7 @@ class TrackTest(ScriptedLoadableModuleTest):
     """Run as few or as many tests as needed here.
     """
     self.setUp()
-    # self.test_load_inputs()
+    self.test_load_inputs()
 
   def test_load_inputs(self):
     """ Test if we can load all inputs
@@ -1223,35 +1223,4 @@ class TrackTest(ScriptedLoadableModuleTest):
     transformationList = self.logic.validateTransformsInput(csv_file_path, total_num_images, csv_headers)
     self.assertTrue(transformationList is not None)
     
-
-    # # Get/create input data
-
-    # import SampleData
-    # registerSampleData()
-    # inputVolume = SampleData.downloadSample('Track1')
-    # self.delayDisplay('Loaded test data set')
-
-    # inputScalarRange = inputVolume.GetImageData().GetScalarRange()
-    # self.assertEqual(inputScalarRange[0], 0)
-    # self.assertEqual(inputScalarRange[1], 695)
-
-    # outputVolume = slicer.mrmlScene.AddNewNodeByClass("vtkMRMLScalarVolumeNode")       
-    # threshold = 100
-
-    # # Test the module logic
-
-    # logic = TrackLogic()
-
-    # # Test algorithm with non-inverted threshold
-    # logic.process(inputVolume, outputVolume, threshold, True)
-    # outputScalarRange = outputVolume.GetImageData().GetScalarRange()
-    # self.assertEqual(outputScalarRange[0], inputScalarRange[0])
-    # self.assertEqual(outputScalarRange[1], threshold)
-
-    # # Test algorithm with inverted threshold
-    # logic.process(inputVolume, outputVolume, threshold, False)
-    # outputScalarRange = outputVolume.GetImageData().GetScalarRange()
-    # self.assertEqual(outputScalarRange[0], inputScalarRange[0])
-    # self.assertEqual(outputScalarRange[1], inputScalarRange[1])
-
-    # self.delayDisplay('Test passed')
+    self.delayDisplay('Test passed')
